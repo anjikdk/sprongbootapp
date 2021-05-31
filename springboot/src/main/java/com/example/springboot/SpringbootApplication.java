2593319264ba7c65d4d3b4b0dd5dc22bbea3aac2
anjikdk <45225583+anjikdk@users.noreply.github.com>
2021-05-31 13:51:28 +0530

    Update SpringbootApplication.java

--- springboot/src/main/java/com/example/springboot/SpringbootApplication.java
+++ springboot/src/main/java/com/example/springboot/SpringbootApplication.java
@@ -2,6 +2,9 @@ package com.example.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class SpringbootApplication {
